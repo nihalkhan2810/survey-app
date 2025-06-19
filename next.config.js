f61ad739157f805a06aa413b0da81c8da7a4c60b
mohammed nihal khan <mohammednihal281001@gmail.com>
2025-06-19 06:07:27 -0400

    again

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   eslint: {
     // Disable ESLint during production builds
     ignoreDuringBuilds: true,
@@ -22,4 +21,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig; 
